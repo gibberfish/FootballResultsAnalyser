179b6479a250306abe19c6bf0731887f43050e9c
Mark Fowler <growlerfowler@talktalk.net>
2014-03-19 21:38:38 +0000

    Implement remaining methods

--- src/test/java/uk/co/mindbadger/footballresults/table/TableRowTest.java
+++ src/test/java/uk/co/mindbadger/footballresults/table/TableRowTest.java
@@ -3,6 +3,8 @@ package uk.co.mindbadger.footballresults.table;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Calendar;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -14,6 +16,7 @@ import uk.co.mindbadger.footballresultsanalyser.domain.Team;
 public class TableRowTest {	
 	private final Integer TEAM_ID = 123;
 	private final String TEAM_NAME = "Portsmouth";
+	private final Calendar FIXTURE_DATE = Calendar.getInstance();
 	
 	private TableRow objectUnderTest;
 	
@@ -24,20 +27,23 @@ public class TableRowTest {
 	private Fixture mockFixture;
 	
 	@Mock
-	private Team mockTeam;
-	
+	private Team mockTeam1;
+
+	@Mock
+	private Team mockTeam2;
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		
-		when (mockTeam.getTeamId()).thenReturn(TEAM_ID);
-		when (mockTeam.getTeamName()).thenReturn(TEAM_NAME);
+		when (mockTeam1.getTeamId()).thenReturn(TEAM_ID);
+		when (mockTeam1.getTeamName()).thenReturn(TEAM_NAME);
 	}
 
 	@Test
 	public void shouldCreateAnEmptyTableRow () {
 		// Given
-		objectUnderTest = new TableRow (mockTeam);
+		objectUnderTest = new TableRow (mockTeam1);
 
 		// When
 
@@ -52,5 +58,33 @@ public class TableRowTest {
 		assertEquals (0, objectUnderTest.getGoalsConceded());
 		assertEquals (0, objectUnderTest.getGoalDifference());
 		assertEquals (0, objectUnderTest.getPoints());
+	}
+	
+	@Test
+	public void shouldCreateATableRowForAHomeTeamWinning () {
+		// Given
+		TableRow previousTableRow = new TableRow (mockTeam1);
+		
+		when (mockFixture.getHomeTeam()).thenReturn(mockTeam1);
+		when (mockFixture.getAwayTeam()).thenReturn(mockTeam2);
+		when (mockFixture.getFixtureDate()).thenReturn(FIXTURE_DATE);
+		when (mockFixture.getHomeGoals()).thenReturn(2);
+		when (mockFixture.getAwayGoals()).thenReturn(1);
+		
+		objectUnderTest = new TableRow (mockTeam1, previousTableRow, mockFixture);
+		
+		// When
+		
+		// Then
+		assertEquals (TEAM_ID, objectUnderTest.getTeamId());
+		assertEquals (TEAM_NAME, objectUnderTest.getTeamName());
+		assertEquals (1, objectUnderTest.getGamesPlayed());
+		assertEquals (1, objectUnderTest.getGamesWon());
+		assertEquals (0, objectUnderTest.getGamesDrawn());
+		assertEquals (0, objectUnderTest.getGamesLost());
+		assertEquals (2, objectUnderTest.getGoalsScored());
+		assertEquals (1, objectUnderTest.getGoalsConceded());
+		assertEquals (1, objectUnderTest.getGoalDifference());
+		assertEquals (3, objectUnderTest.getPoints());
 	}
 }

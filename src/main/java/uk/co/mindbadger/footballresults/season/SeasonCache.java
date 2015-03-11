6eb4075233e4a049c3b66380e53d3c053affa57d
Mark Fowler <mark.fowler@screwfix.com>
2015-03-11 16:04:14 +0000

    Stub classes for the season cache object that will load on startup of the web app

---
+++ src/main/java/uk/co/mindbadger/footballresults/season/SeasonCache.java
@@ -0,0 +1,13 @@
+package uk.co.mindbadger.footballresults.season;
+
+public class SeasonCache {
+	/*
+	 * Aim of this component:
+	 * 
+	 * * Get the latest season by default
+	 * * Get all fixtures for the season
+	 * * Get all divisions for the season
+	 * * For each division, get all of the teams for the season
+	 * * Create a dummy table for 
+	 */
+}
---
+++ src/test/java/uk/co/mindbadger/footballresults/season/SeasonCacheTest.java
@@ -0,0 +1,32 @@
+package uk.co.mindbadger.footballresults.season;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class SeasonCacheTest {
+	
+	private SeasonCache objectUnderTest;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		
+		objectUnderTest = new SeasonCache();
+	}
+
+	@Test
+	public void should() {
+		// Given
+		
+		// When
+		
+		// Then
+	}
+	
+}

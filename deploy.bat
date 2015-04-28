fd6400eb00ef0b6e35ff3d811e94c599053d3226
Mark Fowler <mark.fowler@screwfix.com>
2015-04-28 08:00:34 +0100

    Mark deploy file more generic, now need env var TOMCAT_SERVICE

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,12 @@
 
-net stop "Apache Tomcat 7.0 Tomcat7"
+net stop "%TOMCAT_SERVICE%"
 
 del "%APACHE_HOME%\webapps\football-results-analyser.war"
-
 rmdir /S /q "%APACHE_HOME%\webapps\football-results-analyser"
-rmdir /S /q "%APACHE_HOME%\logs"
 
+rmdir /S /q "%APACHE_HOME%\logs"
 del "C:\Mark\*.log"
 
 copy target\football-results-analyser.war "%APACHE_HOME%\webapps\"
-rem copy H:\dev\football-results-analyser\target\football-results-analyser.war "C:\Program Files\Apache Software Foundation\Tomcat 7.0\webapps\"
 
-net start "Apache Tomcat 7.0 Tomcat7"
+net start "%TOMCAT_SERVICE%"
